dd540152a8b9e89f883a72edec5e06e8737d0677
SachinPatel2707 <sachinpatel2707@gmail.com>
2022-08-30 15:23:54 +0530

    [feature] generate random dataset

---
+++ ExtendibleHashing_2022CSM1012.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class ExtendibleHashing_2022CSM1012
+{
+    public static void main (String[] args) throws IOException
+    {
+        GenerateDataset data = new GenerateDataset();
+        data.generateInitialData();
+    }
+}
---
+++ GenerateDataset.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.Random;
+
+public class GenerateDataset 
+{
+    /*
+     * generates 50,000 random records and stores them in a single text file
+     */
+    public void generateInitialData () throws IOException
+    {
+        File original = new File("original.txt");
+        original.createNewFile();
+        if (original.exists())
+        {
+            Random random = new Random();
+            FileWriter fwrite = new FileWriter(original);
+            for (int index = 1; index <= 500; index++)
+            {
+                int saleAmount = random.nextInt(60001);
+                String customerName = generateRandomString();
+                int category = random.nextInt(1501);
+
+                fwrite.write(index + " " + saleAmount + " " + customerName + " " + category + "\n");
+            }
+            fwrite.close();
+        }
+    }
+
+    public String generateRandomString ()
+    {
+        StringBuilder buffer = new StringBuilder(3);
+        int alphabetStart = 97;
+        int alphabetEnd = 122;
+        for (int i = 0; i < 3; i++)
+        {
+            int randomInt = (int) (Math.random() * (alphabetEnd - alphabetStart + 1)) + alphabetStart;
+            buffer.append((char) randomInt);
+        }
+
+        return (buffer.toString());
+    }
+}
